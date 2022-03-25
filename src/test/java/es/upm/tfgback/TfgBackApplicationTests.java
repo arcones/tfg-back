487ea4af39d488d3cd4b69eb98d02b7d912e26d2
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2022-03-25 18:22:13 +0100

    Initial commit

---
+++ src/test/java/es/upm/tfgback/TfgBackApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.tfgback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
